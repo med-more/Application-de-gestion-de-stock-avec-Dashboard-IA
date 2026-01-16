f0e5e42e0563d3e0c8145b639c682e72fa964419
med-more <mbfmz20@gmail.com>
2026-01-16 20:39:17 +0100

    Add extraReducers for fetchSales pending, fulfilled, and rejected

--- app/store/slices/salesSlice.js
+++ app/store/slices/salesSlice.js
@@ -3,7 +3,7 @@ import axios from "axios"
 
 const API_URL = 'http://localhost:3001'
 
-export const fetchStats = createAsyncThunk(
+export const fetchSales = createAsyncThunk(
     'sales/fetchSales',
     async () => {
         const response = await axios.get(`${API_URL}/sales`)
@@ -19,4 +19,19 @@ const salesSlace = createSlice({
         error: null,
     },
     reducers: {},
+    extraReducers: (builder) => {
+    builder
+      .addCase(fetchSales.pending, (state) => {
+        state.loading = true
+        state.error = null
+      })
+      .addCase(fetchSales.fulfilled, (state, action) => {
+        state.loading = false
+        state.items = action.payload
+      })
+      .addCase(fetchSales.rejected, (state, action) => {
+        state.loading = false
+        state.error = action.error.message
+      })
+  },
 })
