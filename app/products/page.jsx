0fa7bce8e857493990691b8b887267541813e2c9
med-more <mbfmz20@gmail.com>
2026-01-17 18:03:24 +0100

    Set up products page with Redux hooks, layout, and UI imports

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,4 +1,21 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { useRouter } from "next/router"
+import { motion, AnimatePresence } from "framer-motion"
+import Link from "next/link"
+import DashboardLayout from "../components/Layout/DashboardLayout"
+import DataTable from "../components/Table/DataTable"
+import ConfirmModal from "../components/Modal/ConfirmModal"
+import {
+  fetchProducts as fetchProductsAction,
+  deleteProduct,
+} from "../store/slices/productsSlice"
+import {
+  selectAllProducts as selectProducts,
+  selectProductsLoading,
+} from "../store/selectors"
+import { Plus, Edit, Trash2, Search, Filter, X, ChevronDown } from 'lucide-react'
+import toast from 'react-hot-toast'
 
 const page = () => {
   return (
