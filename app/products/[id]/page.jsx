3c912f46edf5bda158c4b289cc693f3ee2fca3af
Mohammed Baba <mbfmz20@gmail.com>
2026-01-20 16:50:49 +0100

    feat: create product details page

--- app/products/[id]/page.jsx
+++ app/products/[id]/page.jsx
@@ -1,4 +1,21 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useRouter, useParams } from 'next/navigation'
+import { motion } from 'framer-motion'
+import Link from 'next/link'
+import DashboardLayout from '../../components/Layout/DashboardLayout'
+import ConfirmModal from '../../components/Modal/ConfirmModal'
+import {
+  fetchProducts as fetchProductsAction,
+  updateProduct,
+  deleteProduct,
+} from '../../store/slices/productsSlice'
+import {
+  selectProductById,
+  selectProductsLoading,
+} from '../../store/selectors'
+import { ArrowLeft, Trash2, Edit } from 'lucide-react'
+import toast from 'react-hot-toast'
 
 const page = () => {
   return (
