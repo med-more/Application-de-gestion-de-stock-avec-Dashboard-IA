a0ed5af4a3fba1ad2b0c24c609c90dec0894b3c7
Mohammed Baba <mbfmz20@gmail.com>
2026-01-23 12:57:04 +0100

    add RootLayout with ReduxProvider and Toaster

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import './globals.css'
+import ReduxProvider from './providers/ReduxProvider'
+import { Toaster } from 'react-hot-toast'
 
-const layout = () => {
-  return (
-    <div>layout</div>
-  )
+export const metadata = {
+  title: 'Dashboard - Gestion de Stock',
+  description: 'Application de gestion de stock avec Dashboard IA',
 }
 
-export default layout
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body>
+        <ReduxProvider>
+          {children}
+          <Toaster position="top-right" />
+        </ReduxProvider>
+      </body>
+    </html>
+  )
+}
